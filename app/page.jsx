f28fe5168d550b5e9ea15251c7a735924213ece0
Yasani Samara Simha Reddy <y.manojreddy6855@gmail.com>
2023-10-13 17:11:46 +0530

    Note Flow Init

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from "@components/Feed";
+
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+           Amazing Notes
+           </span>
+      </h1>
+      <p className="desc text-center">
+        NoteFlow is an open-source note sharing tool for modern world to discover, create and share creative notes.
+      </p>
+
+      <Feed/>
+    </section>
+  )
+}
+
+export default Home
